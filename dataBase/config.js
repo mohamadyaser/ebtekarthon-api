19dedb0e7348ca44b4b4f37f7606162b184bcfbb
Rabab Azazmih <55941943+RA95-png@users.noreply.github.com>
2020-01-02 14:14:13 +0200

    Update DataBase

---
+++ dataBase/config.js
@@ -0,0 +1,24 @@
+const mySql = require('mysql'),
+        express = require("express"),
+      DB_NAME = 'ebtekarthon';
+
+function createDatabaseConnection(callback) {
+    const connection = mySql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '12345',
+        database: DB_NAME
+    });
+    // connect
+    connection.connect((err) => {
+        if (callback) {
+            callback(err, connection);
+        }
+    });
+}
+
+module.exports = {
+    createDatabaseConnection,
+    DB_NAME
+};
+
