44ef221a1c21b7472b06724f39c929e153ac1d6d
Eman-El-Zetawy <dovprogrammer@gmail.com>
2020-01-02 10:17:11 -0800

    1234

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const
     bodyParser = require('body-parser'),
     header = require('./api/header'),
     schedule = require('./api/day'),
-    contact =require('./api/contact'),
+    contact =require('./api/contact'),    
+    partners =require('./api/partners'), 
     db = require('./database/db'),
     port = 3000 ;
 
@@ -13,6 +14,7 @@ app.use(header);
 app.use(db);
 app.use(schedule);
 app.use(contact);
+app.use(partners);
 
 app.listen(port, () => {
     console.log(`server is listening on port ${port}`);
