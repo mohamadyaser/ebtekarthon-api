40c11e73cfca98394e120cc8a96f11e34a3d5931
E2PM <55942164+Eman-El-Zetawy@users.noreply.github.com>
2020-01-02 12:02:02 +0200

    Add files via upload

--- api/contact.js
+++ api/contact.js
@@ -3,20 +3,32 @@ const
     router = express.Router(),
     routeBase = '/contact'
 ;
-
-
-
-router.get(routeBase + '/get', (req, res) => {
-      console.log(req.body);
-    res.send('success get');
+ router.get(routeBase + '/contact', (req, res) => {
+    con.query("SELECT * FROM  `ebtekarthon`.`contact_inf` ", function (err, result) {
+        if (err) throw err ; 
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "*");
+          res.send(result);
+ }); 
 });
-
-// router.get(routeBase + '/contactp', (req, res) => {
-//     res.send('success get');
-// });
-router.post(routeBase + '/post', (req, res) => {
+router.post(routeBase + '/contact', (req, res) => {
    console.log(req.body);
-    res.send('success get');
+   s ="INSERT INTO `ebtekarthon`.`contact_inf` (`name`, `position`, `email`, `bio`) VALUES ( '"
+    + req.body.name +" ', '" + req.body.position +  "', '"
+     + req.body.mobile  +"' , '" + req.body.email + " ' ) ;" ;
+                   
+   db.query(s, function (err, result) {
+      if (err) throw err;
+      let n = { id :result.insertId ,
+         name : req.body.name ,
+          position : req.body.position 
+          , mobile : req.body.mobile ,
+           email : req.body.email  };
+      console.log(result);
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "*")
+  
+ res.send(n);
+ });
 });
-
 module.exports = router;
