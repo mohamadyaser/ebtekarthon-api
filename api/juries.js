09ce6c2701ad8bbada1e460c1034c2ec6d0bca7a
Rabab Azazmih <55941943+RA95-png@users.noreply.github.com>
2020-01-07 12:42:54 +0200

    UPDATE

--- api/juries.js
+++ api/juries.js
@@ -2,30 +2,59 @@ const express = require('express');
 router = express.Router(),
     routeBase = '/juries',
     {createDatabaseConnection, DB_NAME} = require('../dataBase/config.js');
-router.post(routeBase, (req, res) => {
-    createDatabaseConnection((error, connection) => {
-        if (error) {
-            req.status(500);
-            return;
-        }
-        connection.query(`INSERT INTO ${DB_NAME}.day_inf (day_date,ebt_id)
-         VALUES (${req.body.ebt_id},${req.body.day_inf});`, function (err, result) {
+    router.post(routeBase, (req, res) => {
+        createDatabaseConnection((error, connection) => {
+            if (error) {
+                req.status(500);
+                return;
+            }
+            let sql=`INSERT INTO ${DB_NAME}.jurie_inf (name,position,email,bio,img) VALUES ('`+ req.body.name +`' , '`+req.body.position +`' , '`+req.body.email +`' , '`+req.body.BIO +`' , '`+req.body.img + `');`;
+            connection.query(sql, function (err, result) {
             if (err) throw err;
+            let data = { id :result.insertId ,
+                name : req.body.name ,
+                 position : req.body.position ,
+                 email:req.body.email,
+                 BIO:req.body.BIO,
+                 img:req.body.img
+            }
             res.header("Access-Control-Allow-Origin", "*");
             res.header("Access-Control-Allow-Headers", "*");
             console.log(result);
+            console.log(data);
             connection.end();
-            res.send(result);
+            res.send(data);
         });
     });
-})
+    });
+
+    router.delete(routeBase + '/', (req, res) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "*");
+     console.log(req.body);
+     let id=req.body.id;
+     
+     createDatabaseConnection((error, connection) => {
+       if (error) {
+           req.status(500);
+           return;
+       }
+       connection.query(`DELETE FROM ${DB_NAME}.jurie_inf WHERE id IN (`+id  +`)`, function (err, result) {
+        if (err) throw err ; 
+        connection.end();
+        return res.status(201).send(result);
+     }); 
+     });
+     });
+
+
 router.get(routeBase, (req, res) => {
     createDatabaseConnection((error, connection) => {
         if (error) {
             req.status(500)
             return;
         }
-        connection.query("SELECT * FROM  ${DB_NAME}.day_inf ", function (err, result) {
+        connection.query(`SELECT * FROM  ${DB_NAME}.jurie_inf `, function (err, result) {
         if (err) throw err;
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "*");
--- api/speaker.js
+++ api/speaker.js
@@ -29,31 +29,42 @@ router = express.Router(),
             res.send(data);
         });
     });
+    });
 
-
-
-    connection.query(`DELETE * FROM ${DB_NAME}.speaker_inf WHERE id IN (${id})` , function (err, result) {
+    router.delete(routeBase + '/', (req, res) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "*");
+     console.log(req.body);
+     let id=req.body.id;
+     
+     createDatabaseConnection((error, connection) => {
+       if (error) {
+           req.status(500);
+           return;
+       }
+       connection.query(`DELETE FROM ${DB_NAME}.speaker_inf WHERE id IN (`+id  +`)`, function (err, result) {
         if (err) throw err ; 
         connection.end();
         return res.status(201).send(result);
      }); 
+     });
+     });
 
-     
-})
-// router.get(routeBase, (req, res) => {
-//     createDatabaseConnection((error, connection) => {
-//         if (error) {
-//             req.status(500)
-//             return;
-//         }
-//         connection.query(`SELECT * FROM  ${DB_NAME}.speaker_inf `, function (err, result) {
-//         if (err) throw err;
-//         res.header("Access-Control-Allow-Origin", "*");
-//         res.header("Access-Control-Allow-Headers", "*");
-//         console.log(result);
-//         connection.end();
-//         res.send(result);
-//     });
-// });
-// });
+
+router.get(routeBase, (req, res) => {
+    createDatabaseConnection((error, connection) => {
+        if (error) {
+            req.status(500)
+            return;
+        }
+        connection.query(`SELECT * FROM  ${DB_NAME}.speaker_inf `, function (err, result) {
+        if (err) throw err;
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "*");
+        console.log(result);
+        connection.end();
+        res.send(result);
+    });
+});
+});
 module.exports = router;
