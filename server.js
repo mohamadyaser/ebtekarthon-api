dae63f982af9e55f3b7498eff950914149181410
hamzaOly <https://github.com/hamzaOly>
2020-01-12 03:28:40 +0200

    day

--- server.js
+++ server.js
@@ -2,36 +2,26 @@ const
     express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
-   // header = require('./api/header'),
     day = require('./api/day'),
     contact =require('./api/contact'),    
     speaker = require('./api/speaker'),
-    //about = require('./api/about'),
-  //  headerLogo = require('./api/headerLogo'),
-   // home = require('./api/home'),
     event = require('./api/event'),
     socialMedia = require('./api/socialMedia'),
     juries = require('./api/juries'),
     partners = require('./api/partners'),
-  //  db = require('./dataBase/config'),
     cors = require('cors'),
     port = 3000 ;
 
 app.use(bodyParser.json());
 app.use(cors()); 
 
-//app.use(header);
 app.use(day);
 app.use(event);
 app.use(contact);
 app.use(speaker);
-//app.use(about);
-//app.use(headerLogo);
-//app.use(home);
 app.use(socialMedia);
 app.use(juries);
 app.use(partners);
-// app.use(db);
 app.listen(port, () => {
     console.log(`server is listening on port ${port}`);
 });
