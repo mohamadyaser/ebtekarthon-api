5f15a7ce4d902353bf2f4f04b0b4fb95ab842144
hamzaOly <48622633+hamzaOly@users.noreply.github.com>
2020-01-18 19:59:43 +0200

    event full api

---
+++ event.js
@@ -0,0 +1,69 @@
+const express = require('express');
+event = express.Router(),
+	routeBase = '/eventInf', {
+		createDatabaseConnection,
+		DB_NAME
+	} = require('../dataBase/config.js');
+event.post(routeBase, (req, res) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "*");
+	console.log(req.body);
+	
+	
+	createDatabaseConnection((error, connection) => {
+		if (error) {
+			console.log(error);
+			return;
+		}
+		let qu=`INSERT INTO ${DB_NAME}.event_inf (title, time, day_id) VALUES ( '` + req.body.time +"','" +req.body.title + "','" + req.body.day_id + " ');"
+		connection.query(qu, function (err, result) {
+			let data = {
+				time: result.time,
+				title: result.title,
+				day_id: result.day_id
+			};
+			connection.end();
+			res.send(data);
+		});
+	});
+})
+
+
+event.delete(routeBase + '/:id', (req, res) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "*");
+	let id = req.body.id;
+	createDatabaseConnection((error, connection) => {
+		if (error) {
+			req.status(500);
+			return;}
+		connection.query(`DELETE FROM ${DB_NAME}.event_inf WHERE event_id='` + req.params.id + "';", function (err, result) {
+			if (!err)
+				res.send('Deleted..');
+			else
+				console.log(err)
+			connection.end();
+			console.log(id);
+			res.status(201).send(result);
+		});
+	});
+});
+
+
+event.get(routeBase, (req, res) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "*");
+	createDatabaseConnection((error, connection) => {
+		if (error) {
+			console.log(error);
+			return;
+		}
+		connection.query(`SELECT * FROM ${DB_NAME}.event_inf`, function (err, result) {
+			connection.end();
+			console.log(result);
+			res.send(result);
+		});
+	});
+});
+
+module.exports = event;
